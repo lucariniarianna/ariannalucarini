64ce7652450757ebb4c1b459f4f131ea4ce1a78b
lucariniarianna <62288840+lucariniarianna@users.noreply.github.com>
2020-04-01 12:58:37 +0200

    Update R_code_pointpatterns.r

--- R_code_pointpatterns.r
+++ R_code_pointpatterns.r
@@ -2,13 +2,16 @@
 
 # installazione pacchetto spatstat
 install.packages("spatstat")
+# installazione pacchetto rgdal
+install.packages("rgdal")
 
 # richiamo i pacchetti precedentemente installati
 library(ggplot2) # comando alternativo require(ggplot2)
 library(spatstat)
+library(rgdal)
 
 # impostazione della working directory
-setwd("~/lab")
+setwd("~/Documents/lab")
 
 # importazione dei dati
 covid <- read.table("covid_agg.csv", head=T)  #head=T fa capire al sistema che si vuole aggiungere l'intestazione 
@@ -54,7 +57,7 @@ library(spatstat)
 attach(covid)
 
 # tramite la funzione ppp creo un nuovo dataset che mi interessa per l'analisi spaziale
-# covids <- ppp(lon, lat, c(-180,180), c(-90,90))
+covids <- ppp(lon, lat, c(-180,180), c(-90,90))
 
 # semplifico la variabile della densità chiamandola d
 d <- density(covids)
@@ -63,4 +66,32 @@ d <- density(covids)
 plot(d)
 
 # si aggiungono i punti al plot
-point(covids)
+points(covids)
+
+#save the .RData
+
+setwd("~/Documents/lab")
+load("point_pattern.RData")
+ls()
+
+# plot della mappa di densità
+plot(d)
+#palette
+cl <- colorRampPalette(c("yellow","orange","red"))
+plot(d, col= cl)
+
+#plot della mappa della densità dal verde al blu
+cl <- colorRampPalette(c("green","blue","violet"))
+plot(d, col=cl)
+
+# con points si possono inserire i punti spaziali definiti grazie alla funzione ppp
+points(covids)
+
+#
+coastlines <- readOGR ("ne_10m_coastline.shp")
+plot(coastlines, add=T)
+
+#Exercise : plot della mappa di densità con una nuova colorazione e aggiunta delle coastlines
+cl <- colorRampPalette(c("light blue","yellow","red")) (800)
+plot(d, col=cl)
+plot(coastlines, add=T, col="yellow")
