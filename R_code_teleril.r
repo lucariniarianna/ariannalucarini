c8baa924ef8224d529f40af27467ec48dba50460
lucariniarianna <62288840+lucariniarianna@users.noreply.github.com>
2020-04-15 12:57:17 +0200

    Update R_code_teleril.r

--- R_code_teleril.r
+++ R_code_teleril.r
@@ -110,6 +110,122 @@ plotRGB(p224r63_2011,r=5,g=4,b=3,stretch="Lin")
 plotRGB(p224r63_2011,r=6,g=5,b=4,stretch="Lin")
 
 
+# day 2
+
+# scarico e/o richiamo i pacchetti che verranno utilizzati
+install.packages("raster")
+library(raster)
+
+#setto la directory
+setwd("~/Documents/lab")
+load("/Users/ariannalucarini/Documents/lab/Teleril.RData")
+
+ls()
+#importare file all'interno di R
+brick("p224r63_1988_masked.grd")
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+#plot dell'immagine
+plot(p224r63_1988)
+
+#plot di tutte e 4 le bande
+#par ci peremette di utilizzare a blocchi la nostra finestra
+par(mfrow=c(2,2))
+
+#blue
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(p224r63_1988$B1_sre,col=clb)
+
+#green
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(p224r63_1988$B2_sre,col=clg)
+
+#red
+clr <- colorRampPalette(c("dark red","red","pink"))(100)
+plot(p224r63_1988$B3_sre,col=clr)
+
+#near infrared
+clnir <- colorRampPalette(c("red","orange","yellow"))(100)
+plot(p224r63_1988$B4_sre,col=clnir)
+dev.off()
+
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+
+#Exercise : plot the image the nir on the "r" componment in the RGB space
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+
+#Plot dell'immagine 2011 e 1988 per notare le differenze
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+#titolo grafico
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin", main="1988")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin",main="2011")
+
+#la parte agricola è molto più sviluppata rispetto al 1988 (le piante che riflettono il nir sono rosse mentre il suolo agricolo avrà un altro colore
+dev.off()
+
+#spectral index
+#dvi1988=nir1988-red1988
+dvi1988 <- p224r63_1988$B4_sre-p224r63_1988$B3_sre
+# se la pianta è sana il valore sarà alto
+plot(dvi1988)
+
+#spectral index of 2011
+dvi2011 <- p224r63_2011$B4_sre-p224r63_2011$B3_sre
+plot(dvi2011)
+cldvi <- colorRampPalette(c("light blue", "light green", "green")) (100)
+plot(dvi2011, col=cldvi)
+
+#differenza nel tempo dei due indici = analisi multitemporale
+difdvi <- dvi2011-dvi1988
+plot(difdvi)
+cldifdvi <- colorRampPalette(c("red","white","blu"))(100)
+plot(difdvi, col= cldifdvi)
+
+#visualize output
+#multiframe 1988rgb,2011rgb,difdvi
+par(mfrow=c(3,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plot(difdvi, col=cldifdvi)
+
+#changing the grain or resolution
+p224r63_2011lr <- aggregate(p224r63_2011, fact=10)
+p224r63_2011
+p224r63_2011lr
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+
+#lower resoltuzion
+p224r63_2011lr50 <- aggregate(p224r63_2011, fact=50)
+#original 30m -> resampled 1500m
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr50, r=4, g=3, b=2, stretch="Lin")
+
+dv2011lr50 <- p224r63_2011lr50$B$_sre-p224r63_2011lr50$B4_sre
+plot(dv2011lr50)
 
 
+#dvi1988 low resolution
+p224r63_1988lr50 <- aggregate(p224r63_2011, fact=50)
+dvi1988lr50 <- p224r63_1988lr50$B4_sre - p224r63_1988lr50$B3_sre
+#differenza dvi corretta
+difdvilr50 <- dvi2011lr50 - dvi1988lr50
+dvi2011lr50 <- p224r63_2011lr50$B4_sre - p224r63_2011lr50$B3_sre
+dvi1988lr50 <- p224r63_1988lr50$B4_sre - p224r63_1988lr50$B3_sre
+difdvilr50 <- dvi2011lr50 - dvi1988lr50
+plot(difdvilr50,col=cldifdvi)
+
+#multiframe
+par(mfrow=c(2,1))
+plot(difdvi, col=cldifdvi)
+plot(difdvilr50, col=cldifdvi)
+ 
 
